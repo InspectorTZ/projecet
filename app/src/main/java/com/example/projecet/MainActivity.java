4d16152f0b2f4e717d63f9a493b7ea7f4a0aa24f
FirstProject <s393520@guttman.ort.org.il>
2024-03-15 13:12:22 +0200

    l

--- app/src/main/java/com/example/projecet/MainActivity.java
+++ app/src/main/java/com/example/projecet/MainActivity.java
@@ -1,40 +1,28 @@
 package com.example.projecet;
-
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button button11 = findViewById(R.id.button11);
-        Button button12 = findViewById(R.id.button12);
-        setButtonColor(button11, R.color.black);
-        setButtonColor(button12, R.color.black);
+    public class MainActivity extends AppCompatActivity {
 
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    public void onAimButtonClick(View view) {
-        Button clickedButton = (Button) view;
-        int currentColor = (int) clickedButton.getTag();
-
-        if (currentColor == R.color.black) {
-
-            setButtonColor(clickedButton, R.color.white);
-        } else {
-
-            setButtonColor(clickedButton, R.color.black);
         }
+        public void onButtonClick(View view){
+            Button b= (Button) view;
+
+        }
+
+
     }
 
-    private void setButtonColor(Button button, int colorResId) {
-        int color = getResources().getColor(colorResId, getTheme());
-        button.setBackgroundColor(color);
-        button.setTag(colorResId);
-    }
-}
