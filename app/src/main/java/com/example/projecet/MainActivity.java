261da6bb5feb31087444e607ca1bbb24e8b06098
FirstProject <s393520@guttman.ort.org.il>
2024-04-05 13:11:50 +0300

    lo yodea

--- app/src/main/java/com/example/projecet/MainActivity.java
+++ app/src/main/java/com/example/projecet/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.projecet;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.concurrent.ThreadLocalRandom;
+
 
 
 
@@ -20,8 +19,28 @@ import java.util.concurrent.ThreadLocalRandom;
         }
         public void onButtonClick(View view){
             Button b= (Button) view;
+            Drawable backgroundDrawable = b.getBackground();
+
+            if (backgroundDrawable instanceof ColorDrawable)
+            {
+                int color = ((ColorDrawable) backgroundDrawable).getColor();
+                int scor=0;
+                int red = android.graphics.Color.red(color);
+                int green = android.graphics.Color.green(color);
+                int blue = android.graphics.Color.blue(color);
+                scor=scor+1;
+                if (red == 0 && green == 0 && blue == 0) {
+                    System.out.println("SCORE:"+scor);
+                } else {
+                    System.out.println("YOU LOST");
+                }
+            } else {
+                System.out.println("YOU LOST!");
+            }
+
 
         }
+
 
 
     }
